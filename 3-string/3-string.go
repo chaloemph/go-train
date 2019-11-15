29035c6d62d5b2467ed7ee18990ebea1b2bf4478
amang <amang@amangs-MacBook-Pro.local>
2019-11-15 11:00:29 +0700

    update string file

--- 3-string/3-string.go
+++ 3-string/3-string.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	s "strings"
 )
 
 func main() {
+	var p = fmt.Println
 	name := "amang nima"
 	job := " web developer"
 	fmt.Println(name)
@@ -14,4 +16,14 @@ func main() {
 
 	fmt.Println(name, job)
 	fmt.Println(name + job)
+
+	p("Contains : ", s.Contains(job, "web"))
+	p("Count : ", s.Count(job, "web"))
+	p("HasPrefix : ", s.HasPrefix(job, " web"))
+	p("HasSuffix : ", s.HasSuffix(job, "per"))
+	p("Index : ", s.Index(job, "developer"))
+	p("Repeat : ", s.Repeat(job, 5))
+	p("Replace : ", s.Replace(job, "e", "o", -1)) //replace all when match
+	p("Replace : ", s.Replace(job, "e", "o", 1))  //replace 1 when match
+	p("Split : ", s.Split(job, "e"))              //replace 1 when match
 }
